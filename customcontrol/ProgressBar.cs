dc6d881c924ee8851a6eeeecf1fce03bf81bd186
Anant <Anant>
2020-01-06 14:06:15 +0530

    first commit

---
+++ customcontrol/ProgressBar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CustomControl
+{
+    public class ProgressBar : TemplatedView
+    {
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(ProgressBar), null, BindingMode.TwoWay);
+      
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { this.SetValue(TitleProperty, value); }
+        }
+
+
+    }
+}
